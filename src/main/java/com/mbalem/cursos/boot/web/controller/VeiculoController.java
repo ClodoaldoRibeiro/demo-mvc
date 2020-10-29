df5768a6860727c4237ca9fa575f50c88ff8170e
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2020-10-28 23:56:50 -0300

    Update
    Adicionar operações de Editar e Excluir na entidade veículo

--- src/main/java/com/mbalem/cursos/boot/web/controller/VeiculoController.java
+++ src/main/java/com/mbalem/cursos/boot/web/controller/VeiculoController.java
@@ -10,12 +10,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mbalem.cursos.boot.domain.Combustivel;
-import com.mbalem.cursos.boot.domain.Funcionario;
 import com.mbalem.cursos.boot.domain.Marca;
 import com.mbalem.cursos.boot.domain.TiposVeiculos;
 import com.mbalem.cursos.boot.domain.UF;
@@ -62,6 +62,31 @@ public class VeiculoController {
 		veiculoService.Inserir(veiculo);
 		attributes.addFlashAttribute("success", "Veículo inserido com sucesso.");
 		return "redirect:/veiculos/cadastrar";
+	}
+
+	@GetMapping("/editar/{id}")
+	public String moverDadosFormularioEditar(@PathVariable("id") Long id, ModelMap modelMap) {
+		modelMap.addAttribute("veiculo", veiculoService.buscarPorId(id));
+		return "veiculo/cadastro";
+	}
+
+	@PostMapping("/editar")
+	public String editar(@Valid Veiculo veiculo, BindingResult bindingResult, RedirectAttributes attributes) {
+
+		if (bindingResult.hasErrors()) {
+			return "veiculo/cadastro";
+		}
+
+		veiculoService.Alterar(veiculo);
+		attributes.addFlashAttribute("success", "Veículo alterado com sucesso.");
+		return "redirect:/veiculos/cadastrar";
+	}
+
+	@GetMapping("/excluir/{id}")
+	public String excluirPorId(@PathVariable("id") Long id, ModelMap modelMap) {
+		veiculoService.Excluir(id);
+		modelMap.addAttribute("success", "Veículo excluído com sucesso.");
+		return "redirect:/veiculos/listar";
 	}
 
 	@ModelAttribute("marcas")
